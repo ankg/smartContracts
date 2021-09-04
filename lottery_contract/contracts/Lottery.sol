e60074a8883225d22052d20b42de0b1795a9bd99
Aniket <aniketguptaknp@gmail.com>
2021-09-04 23:30:11 +0530

    Add lastWinner and gasPrice

--- lottery_contract/contracts/Lottery.sol
+++ lottery_contract/contracts/Lottery.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 contract Lottery {
     address public manager;
     address[] public players;
+    address public lastWinner;
     
     function Lottery() public {
         manager = msg.sender;
@@ -20,7 +21,8 @@ contract Lottery {
     
     function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(this.balance); 
+        players[index].transfer(this.balance);
+        lastWinner = players[index]; 
         players = new address[](0);
     }
     
