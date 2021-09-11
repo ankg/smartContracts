ac75f9ba73f5cdc70ba606401f0ef63c8db5db8c
Aniket <aniketguptaknp@gmail.com>
2021-09-11 16:36:02 +0530

    Add next routes code

--- kickstarter/routes.js
+++ kickstarter/routes.js
@@ -1,4 +1,6 @@
 const routes = require('next-routes')();
 
-//routes.add()
+routes.add("/campaigns/new","/campaigns/new")
+	  .add("/campaigns/:address", "/campaigns/show");
+
 module.exports = routes;
