b7cf41419000c4062215e5f082849ee7d853c2bf
Aniket <aniketguptaknp@gmail.com>
2021-09-15 18:21:15 +0530

    Add error handling to ContributeForm

--- kickstarter/components/ContributeForm.js
+++ kickstarter/components/ContributeForm.js
@@ -6,13 +6,16 @@ import {Router} from '../routes';
 
 class ContributeForm extends Component {
 	state = {
-		value: ''
+		value: '',
+		errorMessage: '',
+		loading: false
 	};
 
 	onSubmit = async (event) => {
 		event.preventDefault();
 		//loading campaign contract and send it money
 		console.log('chala kuch')
+		this.setState({loading: true, errorMessage: ''});
 		const campaign = Campaign(this.props.address);
 		try {
 			const accounts = await web3.eth.getAccounts();
@@ -22,14 +25,16 @@ class ContributeForm extends Component {
 				  });
 			Router.replaceRoute(`/campaigns/${this.props.address}`)
 		} catch(err) {
-
+			this.setState({ errorMessage: err.message });
 		}
+
+		this.setState({loading: false, value:''});
 	};
 
 	render() {
 		console.log('rendered');
 		return(
-			<Form onSubmit={this.onSubmit}>
+			<Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
 				<Form.Field>
 					<label>Amount to Contribute</label>
 					<Input 
@@ -39,7 +44,8 @@ class ContributeForm extends Component {
 						labelPosition="right"
 					/>
 				</Form.Field>
-				<Button primary>
+				<Message error header="Oops!" content={this.state.errorMessage} />
+				<Button primary loading={this.state.loading}>
 					Contribute!
 				</Button>				
 			</Form>
