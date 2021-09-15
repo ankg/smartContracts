6fde8aacb8529e7c12aeaf88436f1871d3f6b519
Aniket <aniketguptaknp@gmail.com>
2021-09-15 18:22:54 +0530

    Add new routes for requests

--- kickstarter/pages/campaigns/show.js
+++ kickstarter/pages/campaigns/show.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Layout from "../../components/Layout";
 import {Link} from "../../routes";
 import Campaign from '../../ethereum/campaign';
-import {Card, Grid} from 'semantic-ui-react';
+import {Card, Grid, Button} from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from "../../components/ContributeForm";
 
@@ -71,13 +71,25 @@ class CampaignShow extends Component {
 		return(
 		   <Layout>
 				<h3>Campaign Show</h3>
+
 				<Grid>
-					<Grid.Column width={10}>
-						{this.renderCards()}
-					</Grid.Column>
-					<Grid.Column width={6}>
-						<ContributeForm address={this.props.address} />
-					</Grid.Column>					
+					<Grid.Row>
+						<Grid.Column width={10}>
+							{this.renderCards()}
+						</Grid.Column>
+						<Grid.Column width={6}>
+							<ContributeForm address={this.props.address} />
+						</Grid.Column>					
+					</Grid.Row>
+					<Grid.Row>
+						<Grid.Column>
+							<Link route={`/campaigns/${this.props.address}/requests`}>
+								<a>
+									<Button primary>View Requests</Button>
+								</a>
+							</Link>		
+						</Grid.Column>		
+					</Grid.Row>
 				</Grid>
 		   </Layout>
 		); 
